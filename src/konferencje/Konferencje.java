76c35432ed7682886c3e641022c9701bde9aba58
marcin9233 <37576859+marcin9233@users.noreply.github.com>
2019-03-27 20:49:46 +0100

    Add files via upload

--- src/konferencje/Konferencje.java
+++ src/konferencje/Konferencje.java
@@ -10,28 +10,78 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
-public class Konferencje {
-
+public class Konferencje
+{
     static Connection conn = null;
-    
-    public static void main(String[] args) {
+
+	public static void main(String[] args)
+	{
         // TODO code application logic here
-    }
+		Connection connection = null;
+		try
+		{
+			connection = getConnection();
+		}
+		catch (SQLException e)
+		{
+			System.out.println("Cannot connect do database");
+			e.printStackTrace();
+		}
+
+		if(connection!=null)
+			System.out.println("Connected to database");
+	}
+
     public static Connection getConnection() throws SQLException {
-		Properties ustawienia = new Properties();
-		
+
+		Properties ustawienia=new Properties();
+
+		try // try to read from file
+		{
+			ustawienia.load(new FileInputStream("ustawienia.properties"));
+		}
+		catch (IOException e)   // no file exists
+		{
+			System.out.println("No configuration file found!");
+
+			File f = new File("ustawienia.properties");   // create new file and load default configuration
+			try
+			{
+				FileOutputStream output = new FileOutputStream(f);
+				ustawienia.setProperty("userName", "root");
+				ustawienia.setProperty("password", "");
+				ustawienia.setProperty("DBURL", "jdbc:mysql://127.0.0.1:55555/konferencje?useSSL=false&useUnicode=true&useJDBCCompliantTimezoneShift" +
+						"=true&useLegacyDatetimeCode=false&serverTimezone=UTC");
+				ustawienia.store(output, "Database connection paramaters");
+
+				System.out.println("Created configuration file!");
+			}
+			catch (IOException el)
+			{
+				el.printStackTrace();
+			}
+		}
+
+		String DBURL    = (String) ustawienia.get("DBURL");
+		String userName = (String) ustawienia.get("userName");
+		String password = (String) ustawienia.get("password");
+
+		return DriverManager.getConnection(DBURL, userName, password);
+
+		/*
 		ustawienia.setProperty("userName","root");
 		ustawienia.setProperty("password","");
 		ustawienia.setProperty("dbms", "mysql");
 		ustawienia.setProperty("portNumber", "55555");
 		ustawienia.setProperty("connectionProps","konferencje");
 		ustawienia.setProperty("serverName","127.0.0.1");
-		
-		
-		
+
 		try {
 			File domyslne = new File("ustawienia.properties");
 			if (domyslne.isFile()==false) {
+
+				System.out.println("No config file");
+
 				FileOutputStream out = new FileOutputStream("ustawienia.properties");
 				ustawienia.storeToXML(out, null);
 				out.close();
@@ -49,8 +99,6 @@ public class Konferencje {
 			ustawienia.getProperty("portNumber") + "/" + ustawienia.getProperty("connectionProps"),"root",""); 
 	    }
 
-	    return conn;
-	    
-	    
-}
+	    return conn;*/
+	}
 }
